1a3dd8c464bb6d120dd719d138170c80c284dfa1
Qassim Alshakhoori <qalshakhoori@moic.gov.bh>
2019-10-08 10:07:26 +0300

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [HttpGet]
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name="Ferdinand" } ;
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Name="Customer 1" },
+                new Customer { Name="Customer 2"}
+            };
+
+            RandomViewModel viewModel = new RandomViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+    }
+}
